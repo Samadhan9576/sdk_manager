f5fab4831b0edcd1fcfba91347a3740e20f38187
Samadhan Mankar <samadhanmankar2001@gmail.com>
2025-09-28 02:15:03 +0530

    remove unused import

--- app/src/main/java/com/samadhan/sdkmanager/ui/theme/Theme.kt
+++ app/src/main/java/com/samadhan/sdkmanager/ui/theme/Theme.kt
@@ -1,6 +1,5 @@
 package com.samadhan.sdkmanager.ui.theme
 
-import android.app.Activity
 import android.os.Build
 import androidx.compose.foundation.isSystemInDarkTheme
 import androidx.compose.material3.MaterialTheme
